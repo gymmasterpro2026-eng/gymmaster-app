e71278088d03df13aeaf7061aba59879d7b0dcb3
Diego <diegocabrio1805@gmail.com>
2026-08-06 23:51:21 -0300

    Fix blank screen on Android (relative base path)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/gimnasio/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
